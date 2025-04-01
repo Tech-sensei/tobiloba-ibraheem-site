7a5f5ce617093ea26f69a6f1dc4bff271582cf88
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2025-04-01 21:08:29 +0100

    feat: add Timeline component and integrate it into the Home page; update image domains and resume link in About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -55,8 +55,8 @@ const About = () => {
           </div>
 
           <Link
-            // href="/Tobiloba_Ibraheem_CV.pdf"
-            href="/Tobiloba_Ibraheem_resume.pdf"
+            href="/Tobiloba_Ibraheem_CV.pdf"
+            // href="/Tobiloba_Ibraheem_resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="w-fit flex items-center gap-3 rounded-full bg-[#08a97c1a] border border-[#08a97c4d] shadow-md backdrop-blur-md px-4 py-1 transition-all duration-500 ease-in-out group cursor-pointer hover:shadow-md mb-4 md:mb-8"
